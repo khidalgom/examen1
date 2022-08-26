41dc4eb280a6835bcb72707863eaa7e0b765bb14
kenya <kenyahidalgomoran@gmail.com>
2022-08-25 21:48:07 -0500

    Kenya Hidalgo.

---
+++ Kenyahidalgo_examen1.js
@@ -0,0 +1,12 @@
+let hora1=40
+let hora2= 53-hora1;
+let horaordinaria = hora1*265;
+let horaextraordinaria = hora2*350;
+
+if (hora1<=40) {
+    console.log( " el total a pagar por horas ordinarias es: " + horaordinaria);
+    }
+    else {
+
+        console.log( " el total a pagar por horas extraordinarias es:" + horaextraordianaria);
+    }
---
+++ kenyahidalgo_examen2.js
@@ -0,0 +1,3 @@
+for ( let x=660; x<=360; x =20 ) {
+  console.log(x)
+}
